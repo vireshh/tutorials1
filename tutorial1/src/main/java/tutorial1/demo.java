949bd3074d436e9c53676dff68fb336d0281be63
viresh dev <viresh.dev24@gmail.com>
2015-07-13 01:24:37 +0530

    third commit

--- tutorial1/src/main/java/tutorial1/demo.java
+++ tutorial1/src/main/java/tutorial1/demo.java
@@ -11,6 +11,6 @@ public class demo {
 	public static void main(String [] args)
 	{
 		System.out.println("check in");
-		System.out.println("New check in");
+		System.out.println("New check innnnn");
 	}
 }
