c7ba7d0ae0a7f07a1424426cd88839af0872c1df
viresh dev <viresh.dev24@gmail.com>
2015-08-29 01:34:52 +0530

    new changes

--- tutorial1/src/main/java/tutorial1/demo.java
+++ tutorial1/src/main/java/tutorial1/demo.java
@@ -1,16 +1,46 @@
 package tutorial1;
 
 public class demo {
-
+	boolean b = false;
+	NewCl cl = null;
+	String name = "viresh";
+	boolean nameFlag = false;
+	public demo()
+	{
+		cl = new NewCl();
+	}
+	
+	public int judge()
+	{
+		
+		return cl.take();
+	}
 	public boolean getBool()
 	{
 		
-		return false;
+		return b;
 	}
 	
+	public Object checkN()
+	{
+		return null;
+	}
+	
+	public void testCheck()
+	{
+		if(name.equalsIgnoreCase("viresh"))
+		{
+			nameFlag = true;
+		}
+		else
+		{
+			nameFlag = false;
+		}
+		
+	}
 	public static void main(String [] args)
 	{
-		System.out.println("check in");
+		System.out.println("check in git checking in");
 		System.out.println("second check in");
 	}
 }
